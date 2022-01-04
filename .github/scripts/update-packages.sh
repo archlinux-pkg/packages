55e8ddf9c62da16d7edb56eb6325278fda1e4987
MedzikUser <nivua1fn@duck.com>
2022-01-04 19:48:08 +0100

    update

--- .github/scripts/update-packages.sh
+++ .github/scripts/update-packages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e -u
 
 BASEDIR=$(pwd)
 
@@ -23,7 +22,7 @@ do
       continue
     fi
 
-    if [ ! -f "${pkg_dir}/_version.sh" ]
+    if [ ! -f "${pkg_dir}/_version" ]
     then
       latest_tag=$(curl --silent --location -H "Authorization: token ${GITHUB_API_TOKEN}" "https://api.github.com/repos/${pkg_repo}/releases/latest" | jq -r .tag_name)
 
@@ -31,14 +30,12 @@ do
       if [ -z "$latest_tag" ] || [ "${latest_tag}" = "null" ]
       then
         echo "Error: failed to get the latest release tag for '${pkg_dir}'. GitHub API returned 'null' which indicates that no releases available."
-        exit 1
       fi
 
-      ver_a=${latest_tag//[v,r]/}
+      ver_a=${latest_tag#[v,r]}
       version=${ver_a//-/_}
     else
       custom_vars=$(
-        set +e +u
         . "${pkg_dir}/_version"
         echo "latest_tag=${_ver};"
         echo "version=\"${pkgver}\";"
@@ -48,7 +45,12 @@ do
       eval "$custom_vars"
     fi
 
-    if [ pkg_tag != latest_tag ]
+    if [ -z "$latest_tag" ] || [ "${latest_tag}" = "null" ]
+    then
+      echo "Error: failed to get the latest version for '${pkg_dir}'. Version returned 'null'."
+    fi
+
+    if [ "$pkg_tag" != "$latest_tag" ]
     then
       pkg_name=$(basename ${pkg_dir})
 
@@ -56,7 +58,7 @@ do
 
       sed -i "s/^\(_ver=\)\(.*\)\$/\1${latest_tag}/g" "${pkg_dir}/PKGBUILD"
       sed -i "s/^\(pkgver=\)\(.*\)\$/\1${version}/g" "${pkg_dir}/PKGBUILD"
-      sed -i "s/^\(pkgrel=\)\(.*\)\$/\11/g" "${pkg_dir}/PKGBUILD"
+      git diff-index --quiet HEAD || sed -i "s/^\(pkgrel=\)\(.*\)\$/\11/g" "${pkg_dir}/PKGBUILD"
 
       git add ${pkg_dir}
       git commit -m "update '${pkg_name}' to '${latest_tag}'"
