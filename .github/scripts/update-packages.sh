7e8dc343c3b83f3c609df75d7c8f5964638efe0a
MedzikUser <nivua1fn@duck.com>
2022-01-04 19:03:23 +0100

    update scripts

---
+++ .github/scripts/update-packages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e -u
+
+BASEDIR=$(pwd)
+
+for pkg_dir in "${BASEDIR}"/packages/*
+do
+  if [ ! -d "${pkg_dir}/.git" ]
+  then
+    build_vars=$(
+      set +e +u
+      . "${pkg_dir}/PKGBUILD"
+      echo "auto_update=${_auto_update};"
+      echo "pkg_tag=\"${_ver}\";"
+      echo "pkg_repo=\"${_repo}\";"
+    )
+
+    eval "$build_vars"
+
+    # Ignore packages that have auto-update disabled.
+    if [ "${auto_update}" != "true" ]
+    then
+      continue
+    fi
+
+    if [ ! -f "${pkg_dir}/_version.sh" ]
+    then
+      latest_tag=$(curl --silent --location -H "Authorization: token ${GITHUB_API_TOKEN}" "https://api.github.com/repos/${pkg_repo}/releases/latest" | jq -r .tag_name)
+
+      # If the github api returns error
+      if [ -z "$latest_tag" ] || [ "${latest_tag}" = "null" ]
+      then
+        echo "Error: failed to get the latest release tag for '${pkg_dir}'. GitHub API returned 'null' which indicates that no releases available."
+        exit 1
+      fi
+
+      ver_a=${latest_tag//[v,r]/}
+      version=${ver_a//-/_}
+    else
+      custom_vars=$(
+        set +e +u
+        . "${pkg_dir}/_version"
+        echo "latest_tag=${_ver};"
+        echo "version=\"${pkgver}\";"
+        echo "pkg_repo=\"${_repo}\";"
+      )
+
+      eval "$custom_vars"
+    do
+
+    if [ pkg_tag != latest_tag ]
+    then
+      echo "Updating '${pkg_dir}' to '${latest_tag}'."
+
+      sed -i "s/^\(_ver=\)\(.*\)\$/\1${latest_tag}/g"
+      sed -i "s/^\(pkgver=\)\(.*\)\$/\1${version}/g"
+      sed -i "s/^\(pkgrel=\)\(.*\)\$/\11/g"
+
+      git add ${pkg_dir}
+      git commit -m "update ${pkg_dir}"
+      git pull --rebase
+      git push
+    fi
+  fi
+done
