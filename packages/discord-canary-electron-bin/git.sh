1f150c81f88eac01ef391356ce2e1bf490034ecf
MedzikUser <nivua1fn@duck.com>
2022-01-12 23:16:48 +0100

    update

---
+++ packages/discord-canary-electron-bin/git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+_git='https://aur.archlinux.org/discord-canary-electron-bin.git'
+_commit=4ac1f8a44c58d1e732429fa200c3513fae0e5f0a
---
+++ packages/mongodb-compass/git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+_git='https://aur.archlinux.org/mongodb-compass.git'
+_commit=4ac1f8a44c58d1e732429fa200c3513fae0e5f0a
