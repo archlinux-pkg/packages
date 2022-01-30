11c161a698dc8a584e8edec0598ca633fee686ed
MedzikUser <nivua1fn@duck.com>
2022-01-30 22:14:19 +0100

    update

--- .github/scripts/download-packages.sh
+++ .github/scripts/download-packages.sh
@@ -4,6 +4,6 @@ gh release download --repo archlinux-pkg/packages --pattern 'medzikuser.*' --dir
 
 gh release download --repo archlinux-pkg/packages queue --pattern '*' --dir queue
 gh release delete --repo archlinux-pkg/packages queue --yes
-#gh release create --repo archlinux-pkg/packages queue --notes "packages queue" --prerelease
+gh release create --repo archlinux-pkg/packages queue --notes "packages queue" --prerelease
 
 echo
--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -5,5 +5,5 @@ then
   mv queue/* packages/
 
   cd packages
-  repo-add --new --verify --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
+  repo-add --verify --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
 fi
