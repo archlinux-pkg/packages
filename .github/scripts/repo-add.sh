a5e05c5c1410f56bb7e80bb7c3d47a9b79e03097
MedzikUser <nivua1fn@duck.com>
2022-01-30 23:42:12 +0100

    update

--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -5,5 +5,5 @@ then
   mv queue/* pkgs/
 
   cd pkgs
-  repo-add --verify --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
+  repo-add --remove --verify --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
 fi
