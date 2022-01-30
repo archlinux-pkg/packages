c731d3f1841cbad22946baf3cbe242cd10796514
MedzikUser <nivua1fn@duck.com>
2022-01-30 18:20:50 +0100

    update

--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd /home/archlinux-pkg
-
 mv queue/* packages/
 
 cd packages
-
-sudo repo-add --new --remove --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
+repo-add --new --remove --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
