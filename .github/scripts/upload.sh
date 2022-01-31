65b2817c40cbc326a9b5277bdaed3a64e703a8ef
MedzikUser <nivua1fn@duck.com>
2022-01-31 16:37:07 +0100

    update

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
+upload() {
+  local FILE="$1"
+
+  for (( i=0; i<10; i++ ))
+  do
+    gh release upload --repo 'archlinux-pkg/packages' 'packages' "${FILE}" --clobber
+
+    EXIT_STATUS=$?
+    echo "exit: $EXIT_STATUS"
+
+    if ! (( $EXIT_STATUS ))
+    then
+      break
+    else
+      github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
+
+      sleep $(( 10 * (i + 1)))
+    fi
+
+    github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
+  done
+}
+
 if [[ -d "queue" ]]
 then
   for FILE in ./pkgs/*
   do
-    echo "::group::Uploading '${FILE}'"
-
-    for (( i=0; i<10; i++ ))
-    do
-      gh release upload --repo 'archlinux-pkg/packages' 'packages' "${FILE}" --clobber
-
-      EXIT_STATUS=$?
-      echo "exit: $EXIT_STATUS"
-      
-      if ! (( $EXIT_STATUS ))
-      then
-        break
-      else
-        github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
-
-        sleep $(( 10 * (i + 1)))
-      fi
-
-      github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
-    done
-
-    echo "::endgroup::"
+    upload &
   done
-  
-  gh release upload --repo archlinux-pkg/packages packages ./pkgs/* --clobber
 fi
 
