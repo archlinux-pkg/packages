5d6b9a526f6b43e465a9d7a8a2d87021127cb8f2
Oskar <nivua1fn@duck.com>
2022-02-01 22:14:41 +0100

    Update upload.sh

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 upload() {
   local FILE="$1"
+  local TAG="$2"
 
   for (( i=0; i<10; i++ ))
   do
-    github-release upload --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
+    github-release upload --owner 'archlinux-pkg' --repo 'packages' --tag "${TAG}" "${FILE}"
 
     EXIT_STATUS=$?
     echo "exit: $EXIT_STATUS"
@@ -14,21 +14,17 @@ upload() {
     then
       break
     else
-      github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
+      github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag "${TAG}" "${FILE}"
 
       sleep $(( 10 * (i + 1)))
     fi
 
-    github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag 'packages' "${FILE}"
+    github-release delete --owner 'archlinux-pkg' --repo 'packages' --tag "${TAG}" "${FILE}"
   done
 }
 
-if [[ -d "queue" ]]
-then
-  for FILE in ./pkgs/*
-  do
-    upload "${FILE}" &
-    wait
-  done
-fi
-
+for FILE in ./pkgs/*
+do
+  upload "${FILE}" "$1" &
+  wait
+done
