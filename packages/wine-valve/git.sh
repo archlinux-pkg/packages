386638c00fecde3c44de234f74e0f4041f99338a
MedzikUser <nivua1fn@duck.com>
2022-02-09 13:39:31 +0100

    update

--- packages/wine-valve/git.sh
+++ packages/wine-valve/git.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 _git='https://aur.archlinux.org/wine-valve.git'
 _commit=26499e8a777256f806b21d72632ebf5e4baca085
--- packages/yay/git.sh
+++ packages/yay/git.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 _git='https://aur.archlinux.org/yay.git'
 _commit=cdf06b6781263e24d98754a99d70857aa959f691
