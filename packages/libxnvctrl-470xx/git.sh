52612a4321e2227bf0e0def924e0911890eb0d57
MedzikUser <nivua1fn@duck.com>
2022-02-09 22:24:58 +0100

    update

--- packages/libxnvctrl-470xx/git.sh
+++ packages/libxnvctrl-470xx/git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-_git='https://aur.archlinux.org/nvidia-470xx-settings.git'
+_git='https://aur.archlinux.org/libxnvctrl-470xx.git'
 _commit=6f11235a6391006978d6dc848c48cab7ca9e7320
--- packages/nvidia-470xx-utils/git.sh
+++ packages/nvidia-470xx-utils/git.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 _git='https://aur.archlinux.org/nvidia-470xx-utils.git'
 _commit=718702e0aea6661371d91a364e22d4a4006ad7be
