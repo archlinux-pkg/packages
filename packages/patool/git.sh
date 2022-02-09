8b835827f3ae3d11e5b38e6814a7e4200d0aa3bc
MedzikUser <nivua1fn@duck.com>
2022-02-09 13:20:32 +0100

    update

---
+++ packages/patool/git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+_git='https://aur.archlinux.org/patool.git'
+_commit=ad4b5be632aade31d371c1199bbe50a4888f9dd4
---
+++ packages/python-rapidfuzz/git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+_git='https://aur.archlinux.org/python-rapidfuzz.git'
+_commit=c3b0b44259e84a6c068320ca4b46ea792599a3b4
