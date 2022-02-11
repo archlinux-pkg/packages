1ba863bd489a8b379b02506e165a455607662eef
MedzikUser <nivua1fn@duck.com>
2022-02-11 18:10:44 +0100

    update

--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+
+mkdir pkgs_all
+
+for file in pkgs/*
+do
+  touch "pkgs_all/$(basename ${file})"
+done
+
 cd pkgs
 
 rm -f medzikuser.*
 
 repo-add --remove --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
 
-find . -type f -not -name 'medzikuser.*' -delete
+cd ..
+
+diff -q pkgs pkgs_all | grep 'Only in' | grep pkgs_all | awk '{print $4}'
+
+echo asdddddd
+
+deleted=$(diff -q pkgs pkgs_all | grep 'Only in' | grep pkgs_all | awk '{print $4}')
+
+echo ${deleted}
+
+find pkgs -type f -not -name 'medzikuser.*' -delete
