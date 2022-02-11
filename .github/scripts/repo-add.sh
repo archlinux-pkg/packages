14f0837279d5cd6f87bf2f70d25d3b661a9d6a87
MedzikUser <nivua1fn@duck.com>
2022-02-11 22:00:53 +0100

    update

--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${DIR}/connectsftp.sh
 
 mkdir pkgs_all
 
@@ -15,12 +17,11 @@ repo-add --remove --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.x
 
 cd ..
 
-diff -q pkgs pkgs_all | grep 'Only in' | grep pkgs_all | awk '{print $4}'
-
-echo asdddddd
-
 deleted=$(diff -q pkgs pkgs_all | grep 'Only in' | grep pkgs_all | awk '{print $4}')
 
-echo ${deleted}
+connectsftp << EOF
+  cd ${FTP_CWD}
+  rm ${deleted}
+EOF
 
 find pkgs -type f -not -name 'medzikuser.*' -delete
