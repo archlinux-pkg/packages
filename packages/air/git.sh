f8b947e946aad77cdd3b457ce83c477b821a6ff5
Medzik <nivua1fn@duck.com>
2022-02-18 11:53:19 +0100

    update

---
+++ packages/air/git.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+_git='https://aur.archlinux.org/air.git'
+_commit=afc0ce9638dced1a16fa4d35cd9007bc7153c843
---
+++ packages/bottles/git.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+_git='https://aur.archlinux.org/bottles.git'
+_commit=a131da25da59fab8c0ae2f51d172fdcb80590d4f
