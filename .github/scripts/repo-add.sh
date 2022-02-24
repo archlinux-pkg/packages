bdc88ba09367146f9f732a237c7ac72865715ce1
Medzik <nivua1fn@duck.com>
2022-02-24 19:17:30 +0100

    update

--- .github/scripts/repo-add.sh
+++ .github/scripts/repo-add.sh
@@ -3,20 +3,19 @@ TEMPDIR="$(mktemp -d -t medzik-aur-XXXXXXXXXX)"
 
 for file in pkgs/*
 do
-  if [ "$file" != medzikuser.* ]
-  then
-    touch "$TEMPDIR/$(basename ${file})"
-  fi
+  touch "$TEMPDIR/$(basename $file)"
 done
 
 cd pkgs
 
-rm -f medzikuser.*
+#rm -f medzikuser.*
 
 repo-add --new --remove --prevent-downgrade --sign --key 7A6646A6C14690C0 medzikuser.db.tar.xz *.pkg.tar.xz
 
 cd ..
 
+# check what files have been deleted and save the list to `diff.txt`
 diff -q pkgs "$TEMPDIR" | grep 'Only in' | grep "$TEMPDIR" | awk '{print $4}' > diff.txt
 
+# delete all files that are not `medzikuser.*` (database files)
 find pkgs -type f -not -name 'medzikuser.*' -delete
