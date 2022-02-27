f38c6193194ceaf0dfdda2413aa290c085a61ab2
MedzikUser <nivua1fn@duck.com>
2022-02-27 23:28:45 +0100

    add packages...
    - rpi-imager
    - spotify-adblock
    - spotify-tui

---
+++ packages/rpi-imager/git.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+_git='https://aur.archlinux.org/1password.git'
+_commit=a9d5699ad2d35317ee6c34ee5a74fd468f43c83c
---
+++ packages/spotify-tui/git.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+_git='https://aur.archlinux.org/spotify-tui.git'
+_commit=3780735b6a1be4d000af089e228cf2ccc5fe9b96
