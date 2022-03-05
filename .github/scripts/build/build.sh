5aa9a5b8a89e617d98458a19c836fe7e6f325419
MedzikUser <nivua1fn@duck.com>
2022-03-05 21:57:34 +0100

    update build script

--- .github/scripts/build/build.sh
+++ .github/scripts/build/build.sh
@@ -7,11 +7,17 @@ DOCKER_IMAGE="ghcr.io/archlinux-pkg/packages:latest"
 
 mkdir -p \
   "$BUILDDIR" \
-  "$PKGDEST" \
+  "$PKGDEST"
 
+echo "::group::Pull Docker Container"
+sudo docker pull "$DOCKER_IMAGE"
+echo "::endgroup::"
+
+echo "::group::Run Docker Container"
 sudo docker run \
   --mount type=bind,source="$BUILDDIR",target=/mnt/builddir \
   --mount type=bind,source="$PKGDEST",target=/mnt/pkgdest \
   --mount type=bind,source="$SRCDIR",target=/mnt/src \
   "$DOCKER_IMAGE" \
   su -c "/mnt/src/build-package.sh $(cat ./built_packages.txt)" build
+echo "::endgroup::"
