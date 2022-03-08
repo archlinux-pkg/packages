6fdc67160de2b0640ca9bc706e6137dc757df356
MedzikUserBot <rm99iv9s@duck.com>
2022-03-08 21:15:55 +0000

    update 'android-platform-29' to AUR commit '302770c'

--- packages/android-platform-29/git.sh
+++ packages/android-platform-29/git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 _git='https://aur.archlinux.org/android-sdk-build-tools-29.0.2.git'
-_commit='0e870737e7dbed748e97e355e8146d167c0ed6b2'
+_commit='302770c8f595d5db0ea0cc20b07cd7c3b46eb282'
