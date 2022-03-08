10c6d9595aa98b46028e76f345533784426cdf46
Oskar <nivua1fn@duck.com>
2022-03-08 21:20:42 +0000

    fix git url in android-platform-29

--- packages/android-platform-29/git.sh
+++ packages/android-platform-29/git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-_git='https://aur.archlinux.org/android-sdk-build-tools-29.0.2.git'
+_git='https://aur.archlinux.org/android-platform-29.git'
 _commit='302770c8f595d5db0ea0cc20b07cd7c3b46eb282'
