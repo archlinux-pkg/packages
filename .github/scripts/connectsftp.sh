80fe43bceebbe3937520a362b3fdef5310134e23
MedzikUser <nivua1fn@duck.com>
2022-03-15 21:00:57 +0100

    update rsync upload script

--- .github/scripts/connectsftp.sh
+++ .github/scripts/connectsftp.sh
@@ -13,11 +13,11 @@ connectsftp() {
 upload_file() {
   echo "==> Uploading: $@..."
   export SSHPASS="$FTP_PASSWORD"
-  sshpass -e rsync -avL -nPh --ignore-existing $@ -e ssh "${FTP_USER}@${FTP_URI}:${FTP_CWD}/"
+  sshpass -e rsync -avL --ignore-existing $@ -e ssh "${FTP_USER}@${FTP_URI}:${FTP_CWD}/"
 }
 
 upload_file_overwrite() {
   echo "==> Uploading: $@..."
   export SSHPASS="$FTP_PASSWORD"
-  sshpass -e rsync -avL -nPh $@ -e ssh "${FTP_USER}@${FTP_URI}:${FTP_CWD}/"
+  sshpass -e rsync -avL $@ -e ssh "${FTP_USER}@${FTP_URI}:${FTP_CWD}/"
 }
