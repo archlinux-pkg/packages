f7c07a3de9cef4de18de157be7b680994ebc7031
MedzikUser <nivua1fn@duck.com>
2022-03-25 18:42:19 +0100

    update autoupdate scripts

--- scripts/src/rebuild/inputs.ts
+++
@@ -1,9 +0,0 @@
-import { getInput } from "@actions/core"
-
-// check inputs from GitHub Actions or environment variables
-const inputs = {
-  github_token: getInput("token") || process.env.GITHUB_TOKEN,
-  rebuild: getInput("rebuild") || process.env.REBUILD_TYPE || "all",
-}
-
-export default inputs
